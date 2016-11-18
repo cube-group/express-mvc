caba09ad967e575dc719f399c1f4400440f8c12d
linyang <lin2798003@sina.com>
2016-11-18 22:40:59 +0800

    Update AngularEngine.php

--- modules/engine/AngularEngine.php
+++ modules/engine/AngularEngine.php
@@ -3,9 +3,7 @@
  * Created by PhpStorm.
  * User: linyang
  * Date: 16/9/27
- * Time: 下午10:20
  */
-
 namespace engine;
 
 /**
@@ -27,17 +25,17 @@ class AngularEngine extends ViewEngine
         if (empty($data)) {
             echo $htmlData;
         } else {
-            $angularData = '<script>';
-            $angularData .= 'var cubeAngularJSObject = ';
-            $angularData .= json_encode($data);
-            $angularData .= ';</script>';
+            $angularData = '<script>'."\t\n";
+            $angularData .= 'var cubeAngularJSObject = '."\t\n";
+            $angularData .= json_encode($data).";\t\n";
+            $angularData .= '</script>'."\t\n";
 
             if (strstr($htmlData, '<head>')) {
                 $arr = explode('<head>', $htmlData);
-                echo $arr[0] . '<head>' . $angularData . $arr[1];
+                echo $arr[0] . '<head>' ."\t\n". $angularData . $arr[1];
             } else if (strstr($htmlData, '<html>')) {
                 $arr = explode('<html>', $htmlData);
-                echo $arr[0] . '<html><head>' . $angularData . '</head>' . $arr[1];
+                echo $arr[0] . '<html>'."\t\n".'<head>'."\t\n" . $angularData . '</head>'."\t\n" . $arr[1];
             } else {
                 echo $angularData . $htmlData;
             }
